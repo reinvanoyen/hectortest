a695e7649ccf5fdc1deca4322d27578d627ce528
Rein Van Oyen <reinvanoyen@gmail.com>
2017-07-06 13:49:20 +0200

    install adminer

--- setup.sh
+++ setup.sh
@@ -88,6 +88,16 @@ function installComposer {
 	../composer install
 }
 
+function installAdminer {
+
+	echo -e "\n=== Installing adminer ===\n"
+	cd /home/vagrant
+	mkdir adminer 2> /dev/null || echo > /dev/null
+	cd adminer
+	wget -O index.php https://github.com/vrana/adminer/releases/download/v4.3.0/adminer-4.3.0-mysql-en.php
+	wget -O adminer.css https://raw.githubusercontent.com/vrana/adminer/master/designs/pepa-linha/adminer.css
+}
+
 setHostname "development"
 updateRepositories
 installApache
@@ -97,3 +107,4 @@ configureApache
 configurePhp
 restartApache
 installComposer
+installAdminer
